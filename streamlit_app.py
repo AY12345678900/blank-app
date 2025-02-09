d73ffbf73f8762614a64a190f44d9360bceccb60
AY12345678900 <162341871+AY12345678900@users.noreply.github.com>
2025-02-09 19:36:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
-import streamlit as st
+import streamlit as am
 
-st.title("🎈 My new app")
-st.write(
+am.title("Ambulance Aggregation")
+am.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
